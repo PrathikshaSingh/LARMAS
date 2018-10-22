fab907bff4678076cdd2859ae4c85483e3778252
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 21:53:36 +0200

    Update annotations.js

--- tests/annotations.js
+++ tests/annotations.js
@@ -1,11 +1,13 @@
+//Pakages required
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server/app');
 var should = chai.should();
 
+//invoking HTTP function of Chai module
 chai.use(chaiHttp);
 
-
+//testing of each scripted endpoint
 describe('annotations', function() {
   it('should list ALL annotations on /annotations GET', function(done) {
     chai.request(server)
@@ -32,11 +34,8 @@ describe('annotations', function() {
       .end(function(err, res){
         res.should.have.status(200);
         res.should.be.json;
-        res.body.should.be.a('object');
+        res.body.should.be.a('string');
         res.body.should.have.property('SUCCESS');
-        res.body.SUCCESS.should.be.a('object');
-        res.body.SUCCESS.should.have.property('name');
-        res.body.SUCCESS.should.have.property('lastName');
         res.body.SUCCESS.should.have.property('_id');
         res.body.SUCCESS.name.should.equal('Java');
         res.body.SUCCESS.lastName.should.equal('Script');
