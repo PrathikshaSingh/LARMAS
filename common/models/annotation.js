000b9b90192948283cb50a10578a8263259646e4
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 20:43:51 +0200

    Update annotation.js

--- common/models/annotation.js
+++ common/models/annotation.js
@@ -1,5 +1,7 @@
 'use strict';
+//This file is used to validate models
 
 module.exports = function(Annotation) {
-
+  
+  annoation.validatesLengthOf('content', {min: 3, message: {min: 'annoation not valid'}});
 };
