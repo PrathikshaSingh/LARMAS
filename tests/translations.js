30127cd682613e25c559bc9fff72a20b796ac6e4
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 21:59:59 +0200

    Update translations.js

--- tests/translations.js
+++ tests/translations.js
@@ -1,11 +1,12 @@
+//Package required
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server/app');
 var should = chai.should();
-
+//invoking HTTP function of Chai Module
 chai.use(chaiHttp);
 
-
+//testing of each scripted endpoint
 describe('translations', function() {
   it('should list ALL translations on /translations GET', function(done) {
     chai.request(server)
@@ -28,17 +29,16 @@ describe('translations', function() {
   it('should add a SINGLE translations on /translations POST', function(done) {
     chai.request(server)
       .post('/translations')
-      .send({'name': 'Java', 'lastName': 'Script'})
+      .send({'name': 'hello'})
       .end(function(err, res){
         res.should.have.status(200);
         res.should.be.json;
-        res.body.should.be.a('object');
+        res.body.should.be.a('string');
         res.body.should.have.property('SUCCESS');
-        res.body.SUCCESS.should.be.a('object');
+        res.body.SUCCESS.should.be.a('string');
         res.body.SUCCESS.should.have.property('name');
-        res.body.SUCCESS.should.have.property('lastName');
         res.body.SUCCESS.should.have.property('_id');
-        res.body.SUCCESS.name.should.equal('Java');
+        res.body.SUCCESS.name.should.equal('Hello');
         res.body.SUCCESS.lastName.should.equal('Script');
         done();
       });
