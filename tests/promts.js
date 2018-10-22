c101fcdefe62452716ab836e654c4f2acdccf59c
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 21:55:36 +0200

    Update promts.js

--- tests/promts.js
+++ tests/promts.js
@@ -1,11 +1,13 @@
+//Packages required
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server/app');
 var should = chai.should();
 
+//invoking HTTP Function of Chai module
 chai.use(chaiHttp);
 
-
+//testing of each scripted endpoint
 describe('prompts', function() {
   it('should list ALL prompts on /prompts GET', function(done) {
     chai.request(server)
@@ -27,16 +29,15 @@ describe('prompts', function() {
   });
   it('should add a SINGLE prompts on /prompts POST', function(done) {
     chai.request(server)
-      .post('/promptss')
-      .send({'name': 'Java', 'lastName': 'Script'})
+      .post('/prompts')
+      .send({'name': 'Java'})
       .end(function(err, res){
         res.should.have.status(200);
         res.should.be.json;
-        res.body.should.be.a('object');
+        res.body.should.be.a('string');
         res.body.should.have.property('SUCCESS');
-        res.body.SUCCESS.should.be.a('object');
+        res.body.SUCCESS.should.be.a('string');
         res.body.SUCCESS.should.have.property('name');
-        res.body.SUCCESS.should.have.property('lastName');
         res.body.SUCCESS.should.have.property('_id');
         res.body.SUCCESS.name.should.equal('Java');
         res.body.SUCCESS.lastName.should.equal('Script');
