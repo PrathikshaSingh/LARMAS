cfcb8887e3630829b0ec8ab5c5563c640ff45e5a
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 20:49:40 +0200

    Update translation.js

--- common/models/translation.js
+++ common/models/translation.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//This file is used to validate the specific model
 module.exports = function(Translation) {
-
+  translation.validatesLengthOf('content', {min: 5, message: {min: 'transaltion not valid'}});
 };
