b32173b96e87db9cd831e22d59c030e18a6deb85
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 20:42:53 +0200

    Update language.js

--- common/models/language.js
+++ common/models/language.js
@@ -1,7 +1,7 @@
 'use strict';
-
+//This file is used to validate model types
 module.exports = function(Language) {
 
-    //Language.validatesLengthOf('content', {min: 3, message: {min: 'language not valid'}});
+    Language.validatesLengthOf('content', {min: 3, message: {min: 'language not valid'}});
 };
 
