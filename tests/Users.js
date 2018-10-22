0a80208ca3b2f7cc8b4a5101f1d1b4ca1aba208d
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 21:52:20 +0200

    Update Users.js

--- tests/Users.js
+++ tests/Users.js
@@ -1,11 +1,13 @@
+//Packages required
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server/app');
 var should = chai.should();
 
+//invloking HTTP function of Chai Module
 chai.use(chaiHttp);
 
-
+////testing of each scripted endpoint
 describe('Users', function() {
   it('should list ALL Users on /Users GET', function(done) {
     chai.request(server)
@@ -32,11 +34,11 @@ describe('Users', function() {
       .end(function(err, res){
         res.should.have.status(200);
         res.should.be.json;
-        res.body.should.be.a('object');
+        res.body.should.be.a('string');
         res.body.should.have.property('SUCCESS');
         res.body.SUCCESS.should.be.a('object');
-        res.body.SUCCESS.should.have.property('name');
-        res.body.SUCCESS.should.have.property('lastName');
+        res.body.SUCCESS.should.have.property('username');
+        res.body.SUCCESS.should.have.property('email');
         res.body.SUCCESS.should.have.property('_id');
         res.body.SUCCESS.name.should.equal('Java');
         res.body.SUCCESS.lastName.should.equal('Script');
