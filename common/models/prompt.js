8216d05be8d9502a35ddf621dca1605847cdc1d6
PrathikshaSingh <43748806+PrathikshaSingh@users.noreply.github.com>
2018-10-22 20:44:44 +0200

    Update prompt.js

--- common/models/prompt.js
+++ common/models/prompt.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//This file is used to validaet models
 module.exports = function(Prompt) {
-
+  prompt.validatesLengthOf('content', {min: 10, message: {min: 'prompt not valid'}});
 };
